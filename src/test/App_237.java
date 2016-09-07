8cd823078a732e0cce895c105fcc570e86abb7e5
Andrew <mandbat@mail.ru>
2016-09-07 04:12:59 -0600

    237

---
+++ src/test/App_237.java
@@ -0,0 +1,31 @@
+package test;
+
+public class App_237 {
+
+	public static void main(String[] args) {
+
+		ListNode list = new ListNode(1);
+		list.next = new ListNode(2);
+		list.next.next = new ListNode(3);
+		list.next.next.next = new ListNode(4);
+
+		deleteNode(list);
+	}
+
+	public static void deleteNode(ListNode node) {
+
+		node.val = node.next.val;
+		node.next = node.next.next;
+
+	}
+
+	public static class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+
+}
