4a64d3fe98d00c0844e56877bc533b23713ef086
Andrew <mandbat@mail.ru>
2016-09-09 03:54:20 -0600

    169, 242

---
+++ src/test/App_169.java
@@ -0,0 +1,17 @@
+package test;
+
+public class App_169 {
+
+	public static void main(String[] args) {
+		int[] n = { 1, 2, 3, 4, 5, 6 };
+		System.out.println(majorityElement(n));
+	}
+
+	public static int majorityElement(int[] nums) {
+		
+		
+		
+		return 0;
+	}
+
+}
--- src/test/App_242.java
+++ src/test/App_242.java
@@ -1,19 +1,44 @@
 package test;
 
+import java.util.HashMap;
+
 public class App_242 {
 
 	public static void main(String[] args) {
-
 		String s = "anagram";
 		String t = "nagaram";
 		System.out.println(isAnagram(s, t));
-
 	}
 
 	public static boolean isAnagram(String s, String t) {
-		
-		
 
-		return true;
+		if (s == null && t == null)
+			return true;
+		if (s.length() == 0 && t.length() == 0)
+			return true;
+		if (s.length() != t.length())
+			return false;
+
+		char[] chS = s.toCharArray();
+		HashMap<Character, Integer> map = new HashMap<>();
+		for (char c : chS) {
+			if (map.containsKey(c)) {
+				map.put(c, map.get(c) + 1);
+			} else
+				map.put(c, 1);
+		}
+
+		char[] chT = t.toCharArray();
+		for (char c : chT) {
+			if (map.containsKey(c)) {
+				int count = map.get(c);
+				if (count > 1) {
+					map.put(c, count - 1);
+				} else
+					map.remove(c);
+			} else
+				return false;
+		}
+		return (map.size() == 0 ? true : false);
 	}
 }
